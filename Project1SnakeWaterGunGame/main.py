46c7d8594c81ebd0dbe78c050c724ea54275b3ac
Vermas2004d <madhavverma971@gmail.com>
2025-10-21 14:38:26 +0530

    adding the project

---
+++ Project1SnakeWaterGunGame/main.py
@@ -0,0 +1,43 @@
+import random
+
+'''
+1 for snake
+-1 for water
+0  for gun
+'''
+
+computer = random.choice([-1 , 0, 1])
+youstr = input("Enter your choice: ")
+youDict  = {"s": 1 , "w": -1 , "g": 0}
+reverseDict = {1: "Snake" , -1: "Water" , 0: "Gun"}
+
+you = youDict[youstr] 
+
+#By now  we have 2 numbers (variables ), you and computer
+print(f"You chose {reverseDict[you]}\nComputer choose {reverseDict[computer]}")
+
+
+if(computer == you):
+    print("It's a draw")
+else:
+    if(computer == -1 and you == 1): #Ideiology --> (computer - you) == -2
+        print("You win")
+    elif(computer == -1 and you == 0): #-1
+        print("you lose!")
+    elif(computer == 1 and you == 0): # 1
+        print("you win!")
+    elif(computer == 1 and you == -1): # 2
+        print("you lose")
+    elif(computer == 0 and you == -1): # 1
+        print("you win")
+    elif(computer == 0 and you == 1): # -1
+        print("you lose!")
+    else:
+        print("something went wrong!")
+
+
+#Logic based on the ideology:
+# if((computer - you) == -1 or (computer - you ) == 2):
+#     print("You lose")
+# else:
+#     print("You win")
