cf8869900ba75e70c3de0234d9985f1957baa42c
Vermas2004d <madhavverma971@gmail.com>
2025-10-21 13:54:54 +0530

    added the files

--- chapter8/chapter8ps/07_problem7.py
+++ chapter8/chapter8ps/07_problem7.py
@@ -1,1 +1,15 @@
 #Write a python function  to remove a given word from a list  and strip it at the same time..
+
+
+def rem(l, word):
+    n = []
+    for item in l:
+        if not(item == word):
+            n.append(item.strip(word))#strip will remove the heading and trailing word from the item..
+    return n
+    
+l = ["Haanrry" , "Rohan" , "anShubham" , "an"]
+
+print(rem(l, "an"))
+
+
---
+++ chapter8/chapter8ps/08_problem9.py
@@ -0,0 +1,9 @@
+
+
+def mul(num):
+    for i in range(1,11):
+        print(f"{num} X {i} = {num*i}")
+
+
+num = int(input("Enter a number: "))
+mul(num)
